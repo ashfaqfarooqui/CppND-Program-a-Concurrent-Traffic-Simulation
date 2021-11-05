23afdccd03937cdfd0ab9a4ac2959c212aae292a
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2021-11-05 10:35:35 +0100

    Task 4a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -5,17 +5,22 @@
 
 /* Implementation of class "MessageQueue" */
 
-template <typename T> T MessageQueue<T>::receive() {
+TrafficLightPhase MessageQueue::receive() {
   // FP.5a : The method receive should use std::unique_lock<std::mutex>
   // and_condition.wait() to wait for and receive new messages and pull them
   // from the queue using move semantics.
   // The received object should then be returned by the receive function.
 }
 
-template <typename T> void MessageQueue<T>::send(T &&msg) {
+void MessageQueue::send(TrafficLightPhase &&msg) {
   // FP.4a : The method send should use the mechanisms
   // std::lock_guard<std::mutex> as well as _condition.notify_one() to add a new
   // message to the queue and afterwards send a notification.
+  std::lock_guard<std::mutex> lock(_mutex);
+
+  _queue.emplace_back(msg);
+
+  _condition.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
